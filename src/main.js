b874af25722fbd01c03adfbbcb068000120ce80b
jjarv001 <140094219+jjarv001@users.noreply.github.com>
2025-02-08 23:53:34 -0500

    made "add new" windows, needs refinement

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ const { invoke } = window.__TAURI__.core;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-function new_note(object) {
-  invoke("create_note",{object}).then((response) => {
-    if (response == true) {
-      // invoke("save_notes"); save to file, likely json first before deciding
-    } else {
-      //display fail message, maybe print reason
-    }
-  });
-}
 
 function openNav() {
   document.getElementById("theSidenav").style.width = "250px";
@@ -19,3 +10,37 @@ function openNav() {
 function closeNav() {
   document.getElementById("theSidenav").style.removeProperty("width")
 }
+
+// JJ: NEW CODE START:
+
+function openNewFolderDialog() {
+  document.getElementById("newFolderDialog").style.display = "block";
+}
+
+function closeNewFolderDialog() {
+  document.getElementById("newFolderDialog").style.display = "none";
+}
+// THOUGHTS?
+function createNewFolder() {
+  const folderName = document.getElementById("newFolderName").value;
+  invoke("create_new_folder", { folderName }).then(() => {
+    closeNewFolderDialog();
+  });
+}
+
+function openNewNoteDialog() {
+  document.getElementById("newNoteDialog").style.display = "block";
+}
+
+function closeNewNoteDialog() {
+  document.getElementById("newNoteDialog").style.display = "none";
+}
+// THOUGHTS?
+function createNewNote() {
+  const noteName = document.getElementById("newNoteName").value;
+  invoke("create_new_note", { noteName }).then(() => {
+    closeNewNoteDialog();
+  });
+}
+
+// JJ: NEW CODE END
