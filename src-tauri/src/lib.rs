07f4161903da491858ef6efb57d6ea51014f9f4f
TheCSjg <130869096+TheCSjg@users.noreply.github.com>
2025-02-10 19:22:27 -0500

    implement notes display

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 ///Container for backend scripts
 mod note;
 
-use note::{create_note, Note};
+use note::{create_note, get_notes, Note};
+use serde_json::Value;
 use std::{fs, sync::Mutex};
 use tauri::State;
 // use tauri::Manager;
@@ -34,13 +35,40 @@ fn save_data(state: State<NotesState>) {
     fs::write(SAVEFILE, e).expect("Unable to write file");
 }
 
+#[tauri::command]
+fn load_data(state: State<NotesState>) {
+    let mut notes = state.0.lock().expect("could not lock mutex");
+    let file_data = fs::read_to_string(SAVEFILE).expect("should have been able to read the file");
+    let _result = match serde_json::from_str(&file_data){
+        Ok(file) => *notes = file,
+        Err(_error) => {
+            println!("trying to load partial data");
+            if &file_data == "" {
+                println!("File is empty, using default data");
+                return;
+            }
+            let file_json: Value = serde_json::from_str(&file_data).expect("invalid json");
+            if let Some(field) = file_json.get("note_list") {
+                let value = serde_json::from_value(field.clone()).expect("could not extract value of note_list");
+                notes.note_list = value;
+            }
+            println!("successfully loaded partial data")
+        }
+    };
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let notes = Notes::new();
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .manage(NotesState(notes.into()))
-        .invoke_handler(tauri::generate_handler![save_data, create_note])
+        .invoke_handler(tauri::generate_handler![
+            save_data,
+            load_data,
+            create_note,
+            get_notes
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
